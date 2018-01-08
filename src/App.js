f232125d9dc2fc5ce91a4a72b37034728f2dc180
BrownAnthony <chaoticferocity@gmail.com>
2018-01-08 15:06:39 -0600

    finished front layout and components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { Head } from './Navigation';
+import { Cards } from './Card';
+import { Foot } from './Foot';
 import './App.css';
 
 
+
+
+
   class App extends Component {
-    render() {
-      return (
-        <div className="App">
-          <h1>hello</h1>
-        </div>
-      );
+
+      render() {
+        return (
+          <container>
+            <Head />
+            <Cards />
+            <Foot />
+          </container>
+        );
+      }
     }
-  }
 
 export default App;
